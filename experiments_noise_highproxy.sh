029189c186f3b62c273ef6bd849fb71ebaf4b368
gherardo <gherardo.varando@gmail.com>
2023-09-29 06:07:13 +0200

    rm files

--- experiments_noise_highproxy.sh
+++
@@ -1,7 +0,0 @@
-Rscript generate_data.R --outdir data_new --noiseproxy 1 --rep 100 --latents 20 --proxy 1000 --dist exponential,gaussian --noise gaussian,exponential --independent --size 10   
-Rscript generate_data.R --outdir data_new --noiseproxy 1 --rep 100 --latents 20 --proxy 1000 --dist exponential,gaussian --noise gaussian,exponential --independent --size 100       
-Rscript generate_data.R --outdir data_new --noiseproxy 1 --rep 100 --latents 20 --proxy 1000 --dist exponential,gaussian --noise gaussian,exponential --independent --size 1000       
-Rscript generate_data.R --outdir data_new --noiseproxy 1 --rep 100 --latents 20 --proxy 1000 --dist exponential,gaussian --noise gaussian,exponential --independent --size 10000       
-
-
-Rscript run_methods.R --ncl 3 --datadir data_new --outdir results_new
